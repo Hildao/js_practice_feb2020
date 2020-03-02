6cdd9775b25c3d85d5455855607f94dba480e438
Hilda Osazuwa <hildaosazuwa2@yahoo.com>
2020-03-02 14:47:29 +0000

    Solved week2.js challenges

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,43 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
+  if (person.city === "Manchester") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  //Math.ceil() function always rounds a number up to the next largest whole number or integer.
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let sheepCount = 0;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] === "sheep") {
+      sheepCount++;
+    }
+  }
+  return sheepCount;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.address.city === "Manchester" && person.address.postCode.charAt(0).toUpperCase() === "M") {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 module.exports = {
