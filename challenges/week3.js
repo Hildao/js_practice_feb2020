fe8126d87f6e56d848ca80514c1a2b1b66ec2156
Hilda Osazuwa <hildaosazuwa2@yahoo.com>
2020-03-07 18:23:18 +0000

    Updated week3 challenge, solved camelCaseWords, checkIngredients and duplicateNumbers

--- challenges/week3.js
+++ challenges/week3.js
@@ -11,6 +11,11 @@ function getSquares(nums) {
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+  let arr = words.slice(1)
+  let result = []
+  arr.map((word) => result.push(word.charAt(0).toUpperCase() + word.slice(1)))
+
+  return words.length > 1 ? `${words[0]}${result.join('')}` : words.toString()
 }
 
 function getTotalSubjects(people) {
@@ -28,12 +33,18 @@ function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  let menuArr = menu.map((arr) => Object.values(arr.ingredients)).some((i) => i.includes(ingredient));
+  return menuArr
 }
+
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+  //filter arr1 and arr2 then sort the duplicated numbers in ascending order.
+  let duplicate = arr1.filter((item) => arr2.includes(item)).filter((item, index, arr) => arr.indexOf(item) === index).sort();
+  return duplicate;
 }
 
 
